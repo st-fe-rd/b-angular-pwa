d7a63e1339a82e62ea74d9bd8729c65f14abcee8
“at-longnguyen3” <long.nguyen3@asiantech.vn>
2019-06-04 20:36:04 +0700

    Add new line

--- src/app/features/todos/Todo.model.ts
+++ src/app/features/todos/Todo.model.ts
@@ -1,11 +1,10 @@
 export class Todo {
-    id: number;
-    name: string;
-    isCompleted: boolean;
-    constructor(name = ''){
-      this.id = new Date().getTime();
-      this.name = name;
-      this.isCompleted = false;
-    }
+  id: number;
+  name: string;
+  isCompleted: boolean;
+  constructor(name = '') {
+    this.id = new Date().getTime();
+    this.name = name;
+    this.isCompleted = false;
   }
-  
+}
